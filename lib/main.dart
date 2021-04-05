f80346a95db31babce3b490ffc31b20ecac7ae64
joelkoehler <joel61499@gmail.com>
2021-04-05 13:54:16 -0600

    added slide up menu and working font slider. has button placement issue though

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
 import 'package:highlight_text/highlight_text.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 void main() {
   runApp(MyApp());
@@ -80,10 +81,12 @@ class _SpeechScreenState extends State<SpeechScreen> {
 
   @override
   Widget build(BuildContext context) {
+    BorderRadiusGeometry radius = BorderRadius.only(
+      topLeft: Radius.circular(24.0),
+      topRight: Radius.circular(24.0),
+    );
+
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text('Confidence: ${(_confidence * 100.0).toStringAsFixed(1)}%'),
-      // ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
       floatingActionButton: AvatarGlow(
         animate: _isListening,
@@ -97,42 +100,108 @@ class _SpeechScreenState extends State<SpeechScreen> {
           child: Icon(_isListening ? Icons.mic : Icons.mic_none),
         ),
       ),
-      body: Column(
-        //reverse: true,
-        children: [
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 30.0),
-              child: TextHighlight(
-                text: _text,
-                words: _highlights,
-                textStyle: TextStyle(
-                  fontSize: (10 * _fontsize),
-                  color: Colors.black,
-                  fontWeight: FontWeight.w400,
-                ),
+      body: SlidingUpPanel(
+        panel: Center(
+            child: Expanded(
+          child: new Slider(
+            value: _fontsize,
+            activeColor: Colors.blue,
+            inactiveColor: Colors.grey,
+            onChanged: (double s) {
+              setState(() {
+                _fontsize = s;
+              });
+            },
+            divisions: 10,
+            min: 0.0,
+            max: 10.0,
+          ),
+        )),
+        collapsed: Container(
+          decoration:
+              BoxDecoration(color: Colors.blueGrey, borderRadius: radius),
+          child: Center(
+            child: Text(
+              "Swipe for options",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        body: SingleChildScrollView(
+          reverse: true,
+          child: Container(
+            padding: const EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 150.0),
+            child: TextHighlight(
+              text: _text,
+              words: _highlights,
+              textStyle: TextStyle(
+                fontSize: (10 * _fontsize),
+                color: Colors.black,
+                fontWeight: FontWeight.w400,
               ),
             ),
           ),
-          Expanded(
-            child: new Slider(
-              value: _fontsize,
-              activeColor: Colors.blue,
-              inactiveColor: Colors.grey,
-              onChanged: (double s) {
-                setState(() {
-                  _fontsize = s;
-                });
-              },
-              divisions: 10,
-              min: 0.0,
-              max: 10.0,
-            ),
-          )
-        ],
+        ),
+        borderRadius: radius,
       ),
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     // appBar: AppBar(
+  //     //   title: Text('Confidence: ${(_confidence * 100.0).toStringAsFixed(1)}%'),
+  //     // ),
+  //     floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+  //     floatingActionButton: AvatarGlow(
+  //       animate: _isListening,
+  //       glowColor: Theme.of(context).primaryColor,
+  //       endRadius: 75.0,
+  //       duration: const Duration(milliseconds: 2000),
+  //       repeatPauseDuration: const Duration(milliseconds: 100),
+  //       repeat: true,
+  //       child: FloatingActionButton(
+  //         onPressed: _listen,
+  //         child: Icon(_isListening ? Icons.mic : Icons.mic_none),
+  //       ),
+  //     ),
+  //     body: Column(
+  //       //reverse: true,
+  //       children: [
+  //         Expanded(
+  //           child: Container(
+  //             padding: const EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 30.0),
+  //             child: TextHighlight(
+  //               text: _text,
+  //               words: _highlights,
+  //               textStyle: TextStyle(
+  //                 fontSize: (10 * _fontsize),
+  //                 color: Colors.black,
+  //                 fontWeight: FontWeight.w400,
+  //               ),
+  //             ),
+  //           ),
+  //         ),
+  //         Expanded(
+  //           child: new Slider(
+  //             value: _fontsize,
+  //             activeColor: Colors.blue,
+  //             inactiveColor: Colors.grey,
+  //             onChanged: (double s) {
+  //               setState(() {
+  //                 _fontsize = s;
+  //               });
+  //             },
+  //             divisions: 10,
+  //             min: 0.0,
+  //             max: 10.0,
+  //           ),
+  //         )
+  //       ],
+  //     ),
+  //   );
+  // }
 
   void _listen() async {
     if (!_isListening) {
@@ -157,121 +226,3 @@ class _SpeechScreenState extends State<SpeechScreen> {
     }
   }
 }
-
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         // This is the theme of your application.
-//         //
-//         // Try running your application with "flutter run". You'll see the
-//         // application has a blue toolbar. Then, without quitting the app, try
-//         // changing the primarySwatch below to Colors.green and then invoke
-//         // "hot reload" (press "r" in the console where you ran "flutter run",
-//         // or simply save your changes to "hot reload" in a Flutter IDE).
-//         // Notice that the counter didn't reset back to zero; the application
-//         // is not restarted.
-//         primarySwatch: Colors.blue,
-//         // This makes the visual density adapt to the platform that you run
-//         // the app on. For desktop platforms, the controls will be smaller and
-//         // closer together (more dense) than on mobile platforms.
-//         visualDensity: VisualDensity.adaptivePlatformDensity,
-//       ),
-//       home: MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   MyHomePage({Key key, this.title}) : super(key: key);
-
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-
-//   final String title;
-
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           // Column is also a layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Invoke "debug painting" (press "p" in the console, choose the
-//           // "Toggle Debug Paint" action from the Flutter Inspector in Android
-//           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-//           // to see the wireframe for each widget.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headline4,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
