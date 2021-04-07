420eeb7a7dfa3e12c110215cd70d2dad5e548867
joelkoehler <joel61499@gmail.com>
2021-04-07 14:16:00 -0600

    overhauled menu UI, added default welcome text/container, changed button style, updated UI heirarchy

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,8 @@ class _SpeechScreenState extends State<SpeechScreen> {
 
   stt.SpeechToText _speech;
   bool _isListening = false;
-  String _text = 'Press the button and start speaking';
+  String _text = '';
+  bool _welcome = true;
   double _confidence = 1.0;
   double _fontsize;
   double _initfontsize;
@@ -53,9 +54,9 @@ class _SpeechScreenState extends State<SpeechScreen> {
   void initState() {
     super.initState();
     _speech = stt.SpeechToText();
-    _initfontsize = 4;
+    _initfontsize = 3;
     _fontsize = _initfontsize;
-    _darkmode = true;
+    _darkmode = true; // grab from prefs
   }
 
   @override
@@ -77,20 +78,49 @@ class _SpeechScreenState extends State<SpeechScreen> {
           padding: const EdgeInsets.fromLTRB(0.0, 50.0, 0.0, 30.0),
           child: Column(
             children: [
+              Container(
+                padding: const EdgeInsets.fromLTRB(30.0, 0.0, 0.0, 10.0),
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Options",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Color(_darkmode ? 0xFFFFFFFF : 0xFF000000),
+                  ),
+                ),
+              ),
               Padding(
-                padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 40.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Dark Mode",
+                padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+                child: Card(
+                  elevation: 8.0,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  color:
+                      Color(_darkmode ? 0xff424242 : 0xFFFFFFFF), //Colors.grey,
+                  child: ListTile(
+                    onTap: () {
+                      setState(() {
+                        if (_darkmode == true) {
+                          _darkmode = false;
+                        } else {
+                          _darkmode = true;
+                        }
+                        print(_darkmode);
+                      });
+                    },
+                    title: Text(
+                      "Dark theme",
                       style: TextStyle(
-                        fontSize: (18),
                         color: Color(_darkmode ? 0xFFFFFFFF : 0xFF000000),
-                        fontWeight: FontWeight.w400,
+                        fontWeight: FontWeight.w500,
                       ),
                     ),
-                    new Switch(
+                    leading: Icon(
+                      Icons.lightbulb,
+                      color: Color(_darkmode ? 0xFFFFFFFF : 0xFF000000),
+                    ),
+                    trailing: new Switch(
                       value: _darkmode,
                       onChanged: (value) {
                         setState(() {
@@ -101,89 +131,150 @@ class _SpeechScreenState extends State<SpeechScreen> {
                       activeTrackColor: Colors.lightBlue,
                       activeColor: Colors.blue,
                     ),
-                  ],
+                  ),
                 ),
               ),
+              // Container(
+              //   padding: const EdgeInsets.fromLTRB(30.0, 10.0, 0.0, 10.0),
+              //   alignment: Alignment.topLeft,
+              //   child: Text(
+              //     "Text",
+              //     style: TextStyle(
+              //       fontSize: 20.0,
+              //       fontWeight: FontWeight.bold,
+              //       color: Color(_darkmode ? 0xFFFFFFFF : 0xFF000000),
+              //     ),
+              //   ),
+              // ),
+              //
               Padding(
-                padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 5.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Font Size",
-                      style: TextStyle(
-                        fontSize: (18),
-                        color: Color(_darkmode ? 0xFFFFFFFF : 0xFF000000),
-                        fontWeight: FontWeight.w400,
+                padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+                child: Card(
+                  elevation: 8.0,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  color:
+                      Color(_darkmode ? 0xff424242 : 0xFFFFFFFF), //Colors.grey,
+                  child: Column(
+                    children: [
+                      ListTile(
+                        onTap: () {
+                          //open edit profile
+                        },
+                        title: Text(
+                          "Typeface",
+                          style: TextStyle(
+                            color: Color(_darkmode ? 0xFFFFFFFF : 0xFF000000),
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        leading: Icon(
+                          Icons.font_download_outlined,
+                          color: Color(_darkmode ? 0xFFFFFFFF : 0xFF000000),
+                        ),
+                        trailing: Text(
+                          "_fontfamily" + " >",
+                          style: TextStyle(
+                            fontSize: (12),
+                            color: Color(_darkmode ? 0xFFFFFFFF : 0xFF000000),
+                            //fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
-                    ),
-                    Text(
-                      (_fontsize).toStringAsFixed(0),
-                      style: TextStyle(
-                        fontSize: (18),
-                        color: Color(_darkmode ? 0xFFFFFFFF : 0xFF000000),
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 2),
-                child: new Slider(
-                  value: _fontsize,
-                  activeColor: Colors.blue,
-                  inactiveColor: Colors.grey,
-                  onChanged: (double s) {
-                    setState(() {
-                      _fontsize = s;
-                      _initfontsize = _fontsize;
-                    });
-                  },
-                  divisions: 9,
-                  min: 1,
-                  max: 10.0,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 40),
-                child: Center(
-                  child: Text(
-                    "(or pinch/zoom text)",
-                    style: TextStyle(
-                      fontSize: (12),
-                      color: Color(_darkmode ? 0xFFFFFFFF : 0xFF000000),
-                      fontWeight: FontWeight.w400,
-                    ),
+                    ],
                   ),
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Copy text to clipboard",
+                child: Card(
+                  elevation: 8.0,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  color:
+                      Color(_darkmode ? 0xff424242 : 0xFFFFFFFF), //Colors.grey,
+                  child: Column(
+                    children: [
+                      ListTile(
+                        title: Text(
+                          "Font Size",
+                          style: TextStyle(
+                            color: Color(_darkmode ? 0xFFFFFFFF : 0xFF000000),
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        leading: Icon(
+                          Icons.format_size,
+                          color: Color(_darkmode ? 0xFFFFFFFF : 0xFF000000),
+                        ),
+                        trailing: Text(
+                          (_fontsize).toStringAsFixed(0),
+                          style: TextStyle(
+                            fontSize: (18),
+                            color: Color(_darkmode ? 0xFFFFFFFF : 0xFF000000),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 2),
+                        child: new Slider(
+                          value: _fontsize,
+                          activeColor: Colors.blue,
+                          inactiveColor: Colors.grey,
+                          onChanged: (double s) {
+                            setState(() {
+                              _fontsize = s;
+                              _initfontsize = _fontsize;
+                            });
+                          },
+                          divisions: 9,
+                          min: 1,
+                          max: 10.0,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: Center(
+                          child: Text(
+                            "(or pinch/zoom text)",
+                            style: TextStyle(
+                              fontSize: (12),
+                              color: Color(_darkmode ? 0xFFFFFFFF : 0xFF000000),
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+                child: Card(
+                  elevation: 8.0,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  color: Colors.blue[
+                      500], //Colors.blueGrey[300], //Color(_darkmode ? 0xff424242 : 0xFFFFFFFF),
+                  child: ListTile(
+                    onTap: () {
+                      Clipboard.setData(new ClipboardData(text: _text));
+                    },
+                    title: Text(
+                      "Tap to copy text",
                       style: TextStyle(
-                        fontSize: (18),
                         color: Color(_darkmode ? 0xFFFFFFFF : 0xFF000000),
-                        fontWeight: FontWeight.w400,
+                        fontWeight: FontWeight.w500,
                       ),
                     ),
-                    FloatingActionButton(
-                      mini: true,
-                      backgroundColor: Colors.grey[500],
-                      onPressed: () {
-                        Clipboard.setData(new ClipboardData(text: _text));
-                      },
-                      child: Icon(
-                        Icons.content_copy,
-                        size: 22,
-                        //color: Colors.black,
-                      ),
+                    leading: Icon(
+                      Icons.content_copy,
+                      color: Color(_darkmode ? 0xFFFFFFFF : 0xFF000000),
                     ),
-                  ],
+                  ),
                 ),
               ),
             ],
@@ -196,7 +287,7 @@ class _SpeechScreenState extends State<SpeechScreen> {
               padding: const EdgeInsets.fromLTRB(0, 0, 0, 5),
               child: Shimmer.fromColors(
                 baseColor: Colors.white, //Color(0xFFE3F2FD),
-                highlightColor: Colors.blue[200],
+                highlightColor: Colors.pink[50], //Colors.blue[200],
                 child: Column(
                   children: [
                     Icon(
@@ -238,50 +329,72 @@ class _SpeechScreenState extends State<SpeechScreen> {
               _initfontsize = _fontsize;
             });
           },
-          child: Column(
+          child: Stack(
             children: [
-              Expanded(
-                child: SingleChildScrollView(
-                  reverse: true,
-                  child: Container(
-                    //color: Colors.red,
-                    padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
-                    child: TextHighlight(
-                      text: _text,
-                      words: _highlights,
-                      textStyle: TextStyle(
-                        fontSize: (10 * _fontsize),
-                        color: Color(_darkmode ? 0xFFFFFFFF : 0xFF000000),
-                        fontWeight: FontWeight.w400,
+              (_welcome || _text.length < 1)
+                  ? Center(
+                      child: Text(
+                      "hi.",
+                      style: TextStyle(
+                          color: Color(_darkmode ? 0xFFFFFFFF : 0xFF000000),
+                          fontSize: 30),
+                    ))
+                  : Expanded(
+                      child: SingleChildScrollView(
+                        reverse: true,
+                        child: Container(
+                          alignment: Alignment.center,
+                          //color: Colors.red,
+                          padding: const EdgeInsets.fromLTRB(
+                              20.0, 20.0, 20.0, 120.0),
+                          child: TextHighlight(
+                            text: _text,
+                            words: _highlights,
+                            textStyle: TextStyle(
+                              fontSize: (10 * _fontsize),
+                              color: Color(_darkmode ? 0xFFFFFFFF : 0xFF000000),
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-              ),
-              Container(
-                alignment: Alignment.bottomRight,
-                //color: Colors.red,
-                height: 140,
-                //floatingActionButtonLocation: FloatingActionButtonLocation.endDocked, floatingActionButton:
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 55),
-                  child: AvatarGlow(
-                    animate: _isListening,
-                    //glowColor: Theme.of(context).primaryColor,
-                    glowColor: Color(_isListening ? 0xFF44336 : 0xFF2196F3),
-                    endRadius: 75.0,
-                    duration: const Duration(milliseconds: 2000),
-                    repeatPauseDuration: const Duration(milliseconds: 100),
-                    repeat: true,
-                    child: FloatingActionButton(
-                      backgroundColor:
-                          Color(_isListening ? 0xFFF44336 : 0xFF2196F3),
-                      onPressed: _listen,
-                      child: Icon(_isListening ? Icons.mic : Icons.mic_none),
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: Container(
+                  //alignment: Alignment.bottomRight,
+                  //color: Colors.green,
+                  //height: 200,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(60.0, 60.0, 20.0, 30.0),
+                    child: AvatarGlow(
+                      animate: _isListening,
+                      //glowColor: Theme.of(context).primaryColor,
+                      glowColor: Color(_isListening ? 0xFF44336 : 0xFF2196F3),
+                      endRadius: 60.0,
+                      duration: const Duration(milliseconds: 2000),
+                      repeatPauseDuration: const Duration(milliseconds: 100),
+                      repeat: true,
+
+                      child: FloatingActionButton.extended(
+                        backgroundColor:
+                            Color(_isListening ? 0xFFF44336 : 0xFF2196F3),
+                        onPressed: _listen,
+                        label:
+                            Text(_isListening ? "Tap to stop" : "Tap to begin"),
+                      ),
+
+                      // child: FloatingActionButton(
+                      //   backgroundColor:
+                      //       Color(_isListening ? 0xFFF44336 : 0xFF2196F3),
+                      //   onPressed: _listen,
+                      //   child: Icon(_isListening ? Icons.mic : Icons.mic_none),
+                      // ),
                     ),
                   ),
                 ),
-              ),
+              )
             ],
           ),
         ),
@@ -291,6 +404,7 @@ class _SpeechScreenState extends State<SpeechScreen> {
   }
 
   void _listen() async {
+    _welcome = false;
     if (!_isListening) {
       bool available = await _speech.initialize(
         onStatus: (val) => print('onStatus: $val'),
